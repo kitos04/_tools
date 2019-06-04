59ad89ce326dd9f2023138be2d85de8cf4dd3c4b
Nikita Selivanov <kitosan04@gmail.com>
2019-06-04 10:05:19 +0300

    final

--- resize_dialogBox.jsx
+++ resize_dialogBox.jsx
@@ -1,7 +1,7 @@
 #target photoshop
 app.bringToFront();
 
-var ScaleMethod; //percent or pixels
+var ScaleMethod = 2; //percent or pixels
 var ScalePercent;
 var ScaleDimension = new Array();
 var OperatedHeight = 0;
@@ -40,12 +40,12 @@ function main()
 
     var InputText = MainGroup.add ('group');
     InputText.orientation = 'column';
-    var newWidth = InputText.add ("edittext", [0, 0, 50, 20], "1");
-    var newHeight = InputText.add ("edittext", [0, 0, 50, 20], "1");
+    var newWidth = InputText.add ("edittext", [0, 0, 50, 20], "200");
+    var newHeight = InputText.add ("edittext", [0, 0, 50, 20], "200");
     newHeight.visible = false;
 
     var SwitchPixelPercent = win2.add ('dropdownList', [20, 0, 50, 20], ["px","%"]);
-    SwitchPixelPercent.selection = 0;
+    SwitchPixelPercent.selection = 1;
     SwitchPixelPercent.size = [50, 25];
 
     var ButtonsGroups = win.add("group");
@@ -63,7 +63,7 @@ function main()
             Line2.graphics.backgroundColor = Line2.graphics.newBrush (Line2.graphics.BrushType.SOLID_COLOR, [0.1, 0.1, 0.1]);
         }else{
             newHeight.visible = false;
-            newHeight.text = 1; //подтираю высоту, чтобы при отжатой галки невидимое число не брать
+            //newHeight.text = 1; //подтираю высоту, чтобы при отжатой галки невидимое число не брать
             Line1.graphics.backgroundColor = Line1.graphics.newBrush (Line1.graphics.BrushType.SOLID_COLOR, [0.7, 0.7, 0.7]);
             Line2.graphics.backgroundColor = Line2.graphics.newBrush (Line2.graphics.BrushType.SOLID_COLOR, [0.7, 0.7, 0.7]);
         }
@@ -82,10 +82,7 @@ function main()
     {
         OperatedWidth = parseFloat(newWidth.text);
         OperatedHeight = parseFloat(newHeight.text);
-        alert (OperatedHeight + " tut " + OperatedWidth);
-        //var ScaleType = dropdownAlign.selection.text;
         var inputFolder = Folder.selectDialog("Select a folder to resize");
-       // ScalePercent = newWidth.text;
         if (!KeepProportion.value)
         {
             ScaleDimension.length = 0;
@@ -95,7 +92,7 @@ function main()
             ScaleDimension.push(OperatedWidth);
         }
         resize(inputFolder);
-        //win.close();
+        win.close();
     }
 
     Cancel.onClick = function ()
@@ -107,36 +104,69 @@ win.show();
 
 function resize(samplesFolder)
 {
+    OperatedWidth = OperatedWidth/100;
+    OperatedHeight = OperatedHeight/100;
     var fileList = samplesFolder.getFiles();
-    for (var i = 0; i < fileList.length; i++) 
-    {   
-        var file = fileList[i];
-        if (file.name.charAt(0) != '.') 
-        {            
-            if(file instanceof File && file.hidden == false)
-            {
-                if(file.name.match(/\.(psd|png|jpg|jpeg|gif)$/))
-                    {
-                    open(file);
-                    var docWidth = app.activeDocument.width;
-                    if(ScaleDimension.length > 1)
-                    {
-                        app.activeDocument.resizeImage(ScaleDimension[0], ScaleDimension[1]);
-                        app.activeDocument.close(SaveOptions.SAVECHANGES);
-
-                    }else{
-                        app.activeDocument.resizeImage(docWidth * ScaleDimension[0]);
-                        app.activeDocument.close(SaveOptions.SAVECHANGES);
-                        }
-                    }
-                else
-                {
-                    resize(file);
-                }    
+    if (!fileList.length){
+        alert("No files in folder - " + samplesFolder)
+    }else
+        {
+        for (var i = 0; i < fileList.length; i++) 
+        {   
+            var file = fileList[i];
+            if (file instanceof Folder){
+                resize(file);
             }
-        }    
+            if (file.name.charAt(0) != '.') 
+            {            
+                if(file instanceof File && file.hidden == false)
+                {
+                    if(file.name.match(/\.(psd|png|jpg|jpeg|gif)$/))
+                    {
+                        open(file);
+                            if (ScaleMethod == 1){
+                                ResizeFilePixel(file);
+                            }else{
+                                ResizeFilePercent(file);
+                            }
+                    }else
+                    {
+                        resize(file);
+                    }    
+                }
+            }    
+        }
     }
 }
+
+function ResizeFilePixel (inputFile){
+    if(ScaleDimension.length > 1)
+    {
+        app.activeDocument.resizeImage(ScaleDimension[0], ScaleDimension[1]);
+        //app.activeDocument.close(SaveOptions.SAVECHANGES);
+    }else{
+        app.activeDocument.resizeImage(ScaleDimension[0]);
+        //app.activeDocument.close(SaveOptions.SAVECHANGES);
+    }
+}
+
+function ResizeFilePercent (inputFile){
+    var docWidth = app.activeDocument.width;
+    var docHeight = app.activeDocument.height;
+    if(ScaleDimension.length > 1)
+    {
+                alert ("rururru");
+alert ('width = ' + docWidth + OperatedWidth + ', height = ' + docHeight + OperatedHeight);
+        app.activeDocument.resizeImage(docWidth * OperatedWidth, docHeight * OperatedHeight);
+        //app.activeDocument.close(SaveOptions.SAVECHANGES);
+    }else{
+        alert ('width = ' + docWidth + OperatedWidth);
+        app.activeDocument.resizeImage(docWidth * OperatedWidth);
+        alert ("tutut");
+        //app.activeDocument.close(SaveOptions.SAVECHANGES);
+    }
+}
+
 /*
 function main()
 {
