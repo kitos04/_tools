fa0f0911e9d11b815804109b022cb7424b661c20
Nikita Selivanov <kitosan04@gmail.com>
2019-06-04 10:12:52 +0300

    minor change

--- resize_dialogBox.jsx
+++ resize_dialogBox.jsx
@@ -1,14 +1,13 @@
 #target photoshop
 app.bringToFront();
 
-var ScaleMethod = 2; //percent or pixels
+var ScaleMethod = '%'; //percent or pixels
 var ScaleDimension = new Array();
 var OperatedHeight = 0;
 var OperatedWidth = 0;
 main();
 
-function main()
-{
+function main() {
     var win = new Window('dialog', 'Resize Files');
     win.alignChildren = ["center", "center"];
     win.orientation = 'column';
@@ -17,33 +16,33 @@ function main()
     win2.orientation = 'row';
     win2.spacing = 15;
 
-    var MainGroup = win2.add ('group');
+    var MainGroup = win2.add('group');
     MainGroup.graphics.foregroundColor = MainGroup.graphics.newPen(MainGroup.graphics.PenType.SOLID_COLOR, [0.4, 0.4, 0.4], 1);
     MainGroup.spacing = 0;
 
-    var KeepProportionBar = MainGroup.add ('group')
+    var KeepProportionBar = MainGroup.add('group')
     KeepProportionBar.orientation = 'column';
     KeepProportionBar.spacing = 0;
-    var Line1 = KeepProportionBar.add ("panel", [0,0,3,15]);
-    var KeepProportion = KeepProportionBar.add ("checkbox", undefined);
-    var Line2 = KeepProportionBar.add ("panel", [0,15,3,30]);
+    var Line1 = KeepProportionBar.add("panel", [0, 0, 3, 15]);
+    var KeepProportion = KeepProportionBar.add("checkbox", undefined);
+    var Line2 = KeepProportionBar.add("panel", [0, 15, 3, 30]);
     KeepProportion.value = true;
-    Line1.graphics.backgroundColor = Line1.graphics.newBrush (Line1.graphics.BrushType.SOLID_COLOR, [0.7, 0.7, 0.7]);
-    Line2.graphics.backgroundColor = Line2.graphics.newBrush (Line2.graphics.BrushType.SOLID_COLOR, [0.7, 0.7, 0.7]);
+    Line1.graphics.backgroundColor = Line1.graphics.newBrush(Line1.graphics.BrushType.SOLID_COLOR, [0.7, 0.7, 0.7]);
+    Line2.graphics.backgroundColor = Line2.graphics.newBrush(Line2.graphics.BrushType.SOLID_COLOR, [0.7, 0.7, 0.7]);
 
-    var InputDiscription = MainGroup.add ('group');
+    var InputDiscription = MainGroup.add('group');
     InputDiscription.orientation = 'column';
     InputDiscription.alignChildren = ["right", "center"];
-    var WidthTitle = InputDiscription.add ("statictext", undefined, "Width:");
-    var HeightTitle = InputDiscription.add ("statictext", undefined, "Height:");
+    var WidthTitle = InputDiscription.add("statictext", undefined, "Width:");
+    var HeightTitle = InputDiscription.add("statictext", undefined, "Height:");
 
-    var InputText = MainGroup.add ('group');
+    var InputText = MainGroup.add('group');
     InputText.orientation = 'column';
-    var newWidth = InputText.add ("edittext", [0, 0, 50, 20], "200");
-    var newHeight = InputText.add ("edittext", [0, 0, 50, 20], "200");
+    var newWidth = InputText.add("edittext", [0, 0, 50, 20], "200");
+    var newHeight = InputText.add("edittext", [0, 0, 50, 20], "200");
     newHeight.visible = false;
 
-    var SwitchPixelPercent = win2.add ('dropdownList', [20, 0, 50, 20], ["px","%"]);
+    var SwitchPixelPercent = win2.add('dropdownList', [20, 0, 50, 20], ["px", "%"]);
     SwitchPixelPercent.selection = 1;
     SwitchPixelPercent.size = [50, 25];
 
@@ -54,39 +53,36 @@ function main()
     var Cancel = ButtonsGroups.add("button", undefined, "Cancel");
     Cancel.graphics.foregroundColor = Cancel.graphics.newPen(Cancel.graphics.PenType.SOLID_COLOR, [0.5, 0.5, 0.5], 1);
     var SelectButton = ButtonsGroups.add("button", undefined, "Select Folder...");
-    
+
     KeepProportion.onClick = function () {
-        if (!newHeight.visible){
+        if (!newHeight.visible) {
             newHeight.visible = true;
-            Line1.graphics.backgroundColor = Line1.graphics.newBrush (Line1.graphics.BrushType.SOLID_COLOR, [0.1, 0.1, 0.1]);
-            Line2.graphics.backgroundColor = Line2.graphics.newBrush (Line2.graphics.BrushType.SOLID_COLOR, [0.1, 0.1, 0.1]);
-        }else{
+            Line1.graphics.backgroundColor = Line1.graphics.newBrush(Line1.graphics.BrushType.SOLID_COLOR, [0.1, 0.1, 0.1]);
+            Line2.graphics.backgroundColor = Line2.graphics.newBrush(Line2.graphics.BrushType.SOLID_COLOR, [0.1, 0.1, 0.1]);
+        } else {
             newHeight.visible = false;
-            //newHeight.text = 1; //подтираю высоту, чтобы при отжатой галки невидимое число не брать
-            Line1.graphics.backgroundColor = Line1.graphics.newBrush (Line1.graphics.BrushType.SOLID_COLOR, [0.7, 0.7, 0.7]);
-            Line2.graphics.backgroundColor = Line2.graphics.newBrush (Line2.graphics.BrushType.SOLID_COLOR, [0.7, 0.7, 0.7]);
-        }
-    }
-    
-    SwitchPixelPercent.onChange = function ()
-    {
-        if (SwitchPixelPercent.selection.text == "px"){
-            ScaleMethod = 1;
-        }else if (SwitchPixelPercent.selection.text == "%"){
-            ScaleMethod = 2;
+            //newHeight.text = 1; // reset height value
+            Line1.graphics.backgroundColor = Line1.graphics.newBrush(Line1.graphics.BrushType.SOLID_COLOR, [0.7, 0.7, 0.7]);
+            Line2.graphics.backgroundColor = Line2.graphics.newBrush(Line2.graphics.BrushType.SOLID_COLOR, [0.7, 0.7, 0.7]);
         }
     }
 
-    SelectButton.onClick = function ()
-    {
+    SwitchPixelPercent.onChange = function () {
+        if (SwitchPixelPercent.selection.text == "px") {
+            ScaleMethod = 'px';
+        } else if (SwitchPixelPercent.selection.text == "%") {
+            ScaleMethod = '%';
+        }
+    }
+
+    SelectButton.onClick = function () {
         OperatedWidth = parseFloat(newWidth.text);
         OperatedHeight = parseFloat(newHeight.text);
         var inputFolder = Folder.selectDialog("Select a folder to resize");
-        if (!KeepProportion.value)
-        {
+        if (!KeepProportion.value) {
             ScaleDimension.length = 0;
             ScaleDimension.push(OperatedWidth, OperatedHeight);
-        }else{
+        } else {
             ScaleDimension.length = 0;
             ScaleDimension.push(OperatedWidth);
         }
@@ -94,74 +90,60 @@ function main()
         win.close();
     }
 
-    Cancel.onClick = function ()
-    {
+    Cancel.onClick = function () {
         return win.close();
     }
-win.show();
+    win.show();
 }
 
-function resize(samplesFolder)
-{
-    OperatedWidth = OperatedWidth/100;
-    OperatedHeight = OperatedHeight/100;
+function resize(samplesFolder) {
+    OperatedWidth = OperatedWidth / 100;
+    OperatedHeight = OperatedHeight / 100;
     var fileList = samplesFolder.getFiles();
-    if (!fileList.length){
+    if (!fileList.length) {
         alert("No files in folder - " + samplesFolder)
-    }else
-        {
-        for (var i = 0; i < fileList.length; i++) 
-        {   
+    } else {
+        for (var i = 0; i < fileList.length; i++) {
             var file = fileList[i];
-            if (file instanceof Folder){
+            if (file instanceof Folder) {
                 resize(file);
             }
-            if (file.name.charAt(0) != '.') 
-            {            
-                if(file instanceof File && file.hidden == false)
-                {
-                    if(file.name.match(/\.(psd|png|jpg|jpeg|gif)$/))
-                    {
+            if (file.name.charAt(0) != '.') {
+                if (file instanceof File && file.hidden == false) {
+                    if (file.name.match(/\.(psd|png|jpg|jpeg|gif)$/)) {
                         open(file);
-                            if (ScaleMethod == 1){
-                                ResizeFilePixel(file);
-                            }else{
-                                ResizeFilePercent(file);
-                            }
-                    }else
-                    {
+                        if (ScaleMethod == 'px') {
+                            ResizeFilePixel(file);
+                        } else {
+                            ResizeFilePercent(file);
+                        }
+                    } else {
                         resize(file);
-                    }    
+                    }
                 }
-            }    
+            }
         }
     }
 }
 
-function ResizeFilePixel (inputFile){
-    if(ScaleDimension.length > 1)
-    {
+function ResizeFilePixel(inputFile) {
+    if (ScaleDimension.length > 1) {
         app.activeDocument.resizeImage(ScaleDimension[0], ScaleDimension[1]);
         app.activeDocument.close(SaveOptions.SAVECHANGES);
-    }else{
+    } else {
         app.activeDocument.resizeImage(ScaleDimension[0]);
         app.activeDocument.close(SaveOptions.SAVECHANGES);
     }
 }
 
-function ResizeFilePercent (inputFile){
+function ResizeFilePercent(inputFile) {
     var docWidth = app.activeDocument.width;
     var docHeight = app.activeDocument.height;
-    if(ScaleDimension.length > 1)
-    {
-                alert ("rururru");
-alert ('width = ' + docWidth + OperatedWidth + ', height = ' + docHeight + OperatedHeight);
+    if (ScaleDimension.length > 1) {
         app.activeDocument.resizeImage(docWidth * OperatedWidth, docHeight * OperatedHeight);
         app.activeDocument.close(SaveOptions.SAVECHANGES);
-    }else{
-        alert ('width = ' + docWidth + OperatedWidth);
+    } else {
         app.activeDocument.resizeImage(docWidth * OperatedWidth);
-        alert ("tutut");
         app.activeDocument.close(SaveOptions.SAVECHANGES);
     }
 }
